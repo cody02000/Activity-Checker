5fe262d205ad1f4f217e00187224831ea36ef8ef
Cody <williams.c@gmail.com>
2015-07-23 22:49:52 -0500

    Features all work, no errors thrown
    General Settings-done
    PM and Email Settings-done
    Inactivity Check - done
    Activity Check - done
    No Posts Check - done
    PM on inactive group change - done
    Email on no posts member delete - done.

--- Themes/default/ActivityChecker.template.php
+++ Themes/default/ActivityChecker.template.php
@@ -15,14 +15,50 @@
 
 function template_general_settings()
 {	
-	//	Show the confiq_vars.
-	template_show_settings();
+	global $context, $settings, $options, $scripturl, $txt, $modSettings,$themeurl;
+
 	
-	//	Put in a spacer to make it look better.
-	echo '
-	<br />';
+	echo '<form action="' . $scripturl . '?action=admin;area=activity_checker;sa=settings" method="post" name="ActivityCheckerSettings" id="ActivityCheckerSettings" accept-charset="'. $context['character_set'] . '">
+	 <dl class="settings">';
+
+ echo '<dt>
+      <a id="activity_checker_inactive_time" href="' . $scripturl . '?action=helpadmin;help=' . $txt['activity_checker_inactive_time_help'] . '" onclick="return reqWin(this.href);" class="help"><img src="'. $settings['images_url'] .'/helptopics.gif" class="icon" alt="Help"></a> <span><label for="activity_checker_inactive_time">'.$txt['activity_checker_inactive_time'].'</label></span>
+    </dt>
+    <dd>
+      <input type="text" name="activity_checker_inactive_time" id="activity_checker_inactive_time"  value="' . $modSettings['activity_checker_inactive_time'] . '" class="input_text" />
+    </dd>';
+
+echo '	</dl>';
 
 	//	Show the list.
-	template_show_list();
+	template_show_list('activity_checker_membergroup_selector');
+	
+	template_show_list('activity_checker_category_selector');
+	
+	echo '		<div style="text-align: right;"><input type="submit" name="save" value="Save" class="button_submit"></div>
+			<input type="hidden" name="', $context['session_var'], '" value="', $context['session_id'], '" />
+		</div>
+	</form>';
 
+
+}
+
+function template_inactive()
+{
+	template_show_list();
+}
+
+function template_active()
+{
+	template_show_list();
+}
+
+function template_no_posts()
+{
+	template_show_list();
+}
+
+function template_pm_email_settings() {
+	//	Show the confiq_vars.
+	template_show_settings();
 }
